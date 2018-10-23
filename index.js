babbe960a59f433b10d390deca0aac94ec183106
Angela Branaes <angela@proximistyle.com>
2018-10-23 12:50:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,11 +18,11 @@ function destructivelyRemoveFirstDriver() {
 }
 
 function appendDriver(name) {
-  
+  return [...drivers, name]
 }
 
 function prependDriver(name) {
-  
+  return [name, ...drivers]
 }
 
 function removeLastDriver() {
